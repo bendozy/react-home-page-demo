47fdb91c2465b6186f96497bcb4ac54d7ebe87c7
Chidozie Ijeomah <bendozy@gmail.com>
2020-02-05 07:15:51 +0100

    modify action test

--- src/__tests__/actions/auth.js
+++ src/__tests__/actions/auth.js
@@ -4,25 +4,18 @@ import toastr from 'toastr';
 import * as firebase from '../../firebase/firebase';
 import {
   resetPassword,
-  getCurrentUser,
   createUser,
   logoutUser,
   loginUser,
 } from '../../actions/auth';
 
 import {
-  GET_USER_REQUEST,
-  GET_USER_SUCCESS,
-  GET_USER_ERROR,
   LOGIN_USER_REQUEST,
   LOGIN_USER_SUCCESS,
   LOGIN_USER_ERROR,
   LOGOUT_USER_REQUEST,
   LOGOUT_USER_SUCCESS,
   LOGOUT_USER_ERROR,
-  CHANGE_PASSWORD_REQUEST,
-  CHANGE_PASSWORD_SUCCESS,
-  CHANGE_PASSWORD_ERROR,
   CREATE_USER_REQUEST,
   CREATE_USER_SUCCESS,
   CREATE_USER_ERROR,
